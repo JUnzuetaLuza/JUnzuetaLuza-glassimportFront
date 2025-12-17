4e237be5815120a2c0c0ed5ae2b6068a73ee6cc3
JUnzuetaLuza <joseunz2k@gmail.com>
2025-12-16 22:37:57 -0500

    rewrite

--- src/pages/auth/LoginComponent.jsx
+++ src/pages/auth/LoginComponent.jsx
@@ -3,9 +3,34 @@ import { API_BASE_URL } from "../../utils/constans";
 
 export const LoginComponent = ({ handleFlipped }) => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleGoogleLogin = () => {
     window.location.href = `${API_BASE_URL.split('/api')[0]}/oauth2/authorization/google`;
   };
+
+  const handleEmailLogin = (e) => {
+    e.preventDefault();
+
+    fetch(`${API_BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.token) {
+          localStorage.setItem("authToken", data.token);
+          window.location.href = "/panel-citas";
+        }})
+      .catch(error => {
+        console.error("Error during login:", error);
+      });
+
+  }
 
   return (
     <div className="login-card card-front">
@@ -20,10 +45,50 @@ export const LoginComponent = ({ handleFlipped }) => {
         <p className="subtitle">
           ¡Bienvenido! Inicia sesión para acceder al panel de citas.
         </p>
-      <button
-        onClick={handleGoogleLogin}
-        className="google-btn"
-      >
+        {/* <form onSubmit={handleEmailLogin}>
+          <div className="input-group">
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-input" 
+            />
+          </div>
+          <div className="input-group" style={{ marginTop: "15px" }}>
+            <input
+              type="password"
+              placeholder="Contraseña"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-input"
+            />
+          </div>
+          
+          <button type="submit" className="login-btn" style={{ marginTop: "20px", marginBottom: "20px" }}>
+            Iniciar Sesión
+          </button>
+        </form>
+
+        <p className="register-text" style={{ marginTop: "25px" }}>
+
+          <span
+            onClick={handleFlipped}
+            id="show-register"
+            className="register-link"
+            >
+          </span>
+
+        </p>
+        
+        <p className="or-separator" style={{ textAlign: "center", margin: "10px 0" }}>o</p> */}
+
+        <button
+          onClick={handleGoogleLogin}
+          className="google-btn"
+        >
         <img
           src="https://developers.google.com/identity/images/g-logo.png"
           alt="Google logo"
